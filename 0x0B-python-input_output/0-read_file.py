5d338b4ba2ba384709ac0bcd911a0a251a04ac90
Zayyhan <Zayyhanb2@gmail.com>
2023-01-11 11:32:36 -0800

    input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 
 
 def read_file(filename=""):
-    """reads a text file and prints it to stdout
-    """
     with open('filename', encoding="utf-8") as myFile:
         print(myFile.read(), end='')
+        """reads a text file and prints it to stdout
+        """
